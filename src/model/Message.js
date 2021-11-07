9b1e90d3a2c26e1317e8c22155a237cae5bfdf0e
Orlandinho <horlandomagic@gmail.com>
2021-11-07 15:50:54 -0300

    Image Uploading

--- src/model/Message.js
+++ src/model/Message.js
@@ -96,7 +96,7 @@ export class Message extends Model {
                                         </div>
                                     </div>
                                 </div>
-                                <img src="#" class="_1JVSX message-photo" style="width: 100%; display:none">
+                                <img src="${this.content}" class="_1JVSX message-photo" style="width: 100%; display:none">
                                 <div class="_1i3Za"></div>
                             </div>
                             <div class="message-container-legend">
@@ -121,6 +121,16 @@ export class Message extends Model {
                     </div>
                 </div>
             `;
+
+            div.querySelector('.message-photo').on('load', e => {
+
+                div.querySelector('.message-photo').show();
+                div.querySelector('._34Olu').hide();
+                div.querySelector('._3v3PK').css({
+                    height: 'auto'
+                });
+            });
+
             break;
 
             case 'document':
@@ -280,6 +290,35 @@ export class Message extends Model {
         div.firstElementChild.classList.add(className);
 
         return div;
+    }
+
+    static sendImage(chatId, from, file){
+
+        return new Promise((s, f) => {
+
+            let uploadTask = Firebase.hd().ref(from).child(Date.now() + '_' + file.name).put(file);
+
+            uploadTask.on('state_changed', e => {
+
+                console.info('upload', e);
+            }, err => {
+
+                console.error(err);
+            }, () => {
+
+                uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
+                    Message.send(
+                        chatId, 
+                        from, 
+                        'image', 
+                        downloadURL                    
+                    ).then(() => {
+                        s();
+                    });
+                });
+            });
+        });
+
     }
 
     static send(chatId, from, type, content) {
